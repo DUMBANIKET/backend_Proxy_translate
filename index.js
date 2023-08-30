ca9c43a7b914f7609e4e02aca791fe107c0ab601
Aniket Sen <aniket.singh21@vsit.edu.in>
2023-08-30 15:16:47 +0000

    adding sentry backup

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const axios = require('axios').default;
     const { v4: uuidv4 } = require('uuid');
     require('dotenv').config()
+    const Sentry=require('@sentry/node')
     const express=require("express")
     const cors=require('cors')
-    const app=express()
+    const app=express();
+    Sentry.init({
+        dsn: "https://635dd3a4826268be265a69c28012ab76@o4504427949916160.ingest.sentry.io/4505794520809472",
+        integrations: [
+          // enable HTTP calls tracing
+          new Sentry.Integrations.Http({
+            tracing: true
+          }),
+          // enable Express.js middleware tracing
+          new Sentry.Integrations.Express({
+            app
+          }),
+        ],
+        // Performance Monitoring
+        tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!,
+      });
+      
+      // Trace incoming requests
+      app.use(Sentry.Handlers.requestHandler());
+      app.use(Sentry.Handlers.tracingHandler());
     // const bodyparser=require('body-parser')
     app.use(express.json())
     app.use(cors({
@@ -51,6 +71,18 @@ res.send(JSON.stringify(response.data, null, 4))
         console.log(JSON.stringify(response.data, null, 4));
     })
 })
+
+app.use(Sentry.Handlers.errorHandler());
+
+
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
 
 
 app.listen(5000,()=>{
